7e947b14fe3a2c7133d49aee9dd73d2237986866
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-07-20 14:09:02 -0700

    now accepts formatter functions

--- src/useAxios.js
+++ src/useAxios.js
@@ -2,12 +2,13 @@ import { useState } from "react";
 import axios from "axios";
 import {v1 as uuid} from "uuid";
 
-function useAxios(baseURL) {
+function useAxios(baseURL, format=(a) => a) {
     const [arr, setArr] = useState([])
     
     const requestAndAddToArr = async function(suffix = "") {
         const response = await axios.get(suffix ? `${baseURL}/${suffix}` : baseURL);
-        setArr(arr => [...arr, {...response.data, id: uuid() }])
+        const data = format(response.data);
+        setArr(arr => [...arr, {...data, id: uuid() }])
     }
     
     const clear = function () {
